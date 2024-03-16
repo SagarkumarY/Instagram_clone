d008213bea0c4b430f3ca22f6e190a434a6a7d41
Sagar <sagaryadav47232@gmail.com>
2024-03-16 11:35:48 +0530

    remove comments firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -24,22 +24,3 @@ export { auth, firestore, storage, app };
 
 
 
-
-/// bEFOR CHANGE
-
-// apiKey: "AIzaSyC29vWoDWY2TbXfavYUSbTS8QW6dS8thOM",
-//   authDomain: "insta-clone-79512.firebaseapp.com",
-//   projectId: "insta-clone-79512",
-//   storageBucket: "insta-clone-79512.appspot.com",
-//   messagingSenderId: "357944656047",
-//   appId: "1:357944656047:web:0afed1b64e82dead67945a"
-
-
-// After change
-
-// apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID
